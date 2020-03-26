01583191c9a5d87648b4c391573f9da441c67317
banicola <bastien.nicolas@student.unamur.be>
2020-03-26 18:13:56 +0100

    Login fonctionnel v1

--- src/app/header-admin/header-admin.component.ts
+++ src/app/header-admin/header-admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthentificationService } from '../_services/authentification.service';
+import { User } from '../_models/user';
+
+import * as jwt_decode from "jwt-decode";
 //import {MatDialog, MatDialogConfig} from '@angular/material/dialog'
 //import {LoginComponent} from '../login/login.component'
 
@@ -10,7 +13,9 @@ import { AuthentificationService } from '../_services/authentification.service';
 })
 export class HeaderAdminComponent implements OnInit {
   isAuthenticated: boolean;
+  data;
   constructor(public authService: AuthentificationService) {
+    this.data = localStorage.getItem('currentUser');
   }
 
   ngOnInit(): void {
@@ -19,6 +24,20 @@ export class HeaderAdminComponent implements OnInit {
   isLoggedIn() {
     this.isAuthenticated = this.authService.isAuthenticated();
     return this.isAuthenticated;
+  }
+
+  getCurrentUser<User>(){
+    var currentUser = this.getDecodedAccessToken(this.data);
+    return currentUser.firstname+" "+currentUser.lastname;
+  }
+
+  getDecodedAccessToken(token: string): User {
+    try{
+        return jwt_decode(token);
+    }
+    catch(Error){
+        return null;
+    }
   }
  /* openModal(){
     public matDialog: MatDialog
